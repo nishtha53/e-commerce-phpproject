f597d0091802febe1f182a3c080cb488c06417dd
nishtha53 <68805896+nishtha53@users.noreply.github.com>
2023-01-05 00:57:17 +0530

    about us page designed

---
+++ about.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    <title>E-commerce Project</title>
+</head>
+
+<body>
+    <nav class="navbar  navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="home.php"><img src="images/logo.jpeg" height="80px" width="150px" /> </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="main_nav">
+
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item active"> <a class="nav-link mx-4" href="home.php">Home </a> </li>
+                <li class="nav-item"><a class="nav-link mx-4" href="about.php"> About </a></li>
+                <li class="nav-item"><a class="nav-link mx-4" href="#">Contact us</a></li>
+
+            </ul>
+            <ul style="list-style: none;">
+                <li class="nav-item"><a class="nav-link" href="#"><img src="images/add-to-basket.png" width="40px" height="40px"></a></li>
+            </ul>
+            <a class="btn btn-warning mx-4" href="#">Login</a>
+            <a class="btn btn-warning mx-4" href="#">Register</a>
+        </div> <!-- navbar-collapse.// -->
+    </nav>
+
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Welcome to Medico</h1><br>
+                    <p>Medico shop- Your Health & Fitness Partner is an online portal for healthcare products brought to you by Medicare Products Inc. The company is managed by a team of two young & motivated entrepreneurs with an objective to provide and extend personalized and innovative healthcare services to its customers.</p>
+
+                    <p>Medico Shop is a brainchild of our Founder who aims to provide its customers an easy access to all healthcare needs beyond physical boundaries in the comfort of their homes.</p>
+                </div>
+                <div class="col">
+                    <img src="./images/about-medicine.png" alt="medicine" id="image">
+                </div>
+            </div>
+    </section>
+    <br><br><br><br>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-6 col-md-4">
+                    <div class="card" style="width: 22rem;">
+                        <img class="card-img-top" src="./images/HJ.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">As the information age unfolds and a borderless world emerges we have a quest to become the most admired group. By encouraging innovation and nurturing intellectual capital, we seek quantum growth to lead in the domestic market. We aim to continually enhance value for our customers with quality products and services to meet their ever changing needs.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-md-4">
+                    <div class="card" style="width: 22rem;">
+                        <img class="card-img-top" src="./images/promise.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Keeping in mind our responsibility towards humanity, we promise:To deliver 100% genuine and quality productsTo deliver the products at the earliest for the benefit of our customers To follow all the guidelines and norms which have been laid down.To serve you to the best of our ability today, tomorrow and always.To serve you the best with affordability.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-md-4">
+                    <div class="card" style="width: 22rem;">
+                        <img class="card-img-top" src="./images/mission.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Medico aims to help you manage your day to day requirement with:
+                            <ul>
+                                <li>Ease</li>
+                                <li>Trust</li>
+                                <li>Comfort</li>
+                                <li>Convenience</li>
+                                <li>Affordability</li>
+                            </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section><br><br>
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <h6>About</h6>
+                    <p class="text-justify">Medico shop- Your Health & Fitness Partner is an online portal for healthcare products brought to you by Medicare Products Inc. The company is managed by a team of two young & motivated entrepreneurs with an objective to provide and extend personalized and innovative healthcare services to its customers.</p>
+                </div>
+
+                <div class="col-xs-6 col-md-3">
+                    <h6>Categories</h6>
+                    <ul class="footer-links">
+                        <li><a href="#">Syrup</a></li>
+                        <li><a href="#">Tablet</a></li>
+                        <li><a href="#">Ayurveda</a></li>
+                        <li><a href="#">Hompyopethik</a></li>
+                        <li><a href="#">Emulsions</a></li>
+                        <li><a href="#">Inhalers</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-xs-6 col-md-3">
+                    <h6>Quick Links</h6>
+                    <ul class="footer-links">
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+                        <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-sm-6 col-xs-12">
+                    <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by
+                        <a href="#">Scanfcode</a>.
+                    </p>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <ul class="social-icons">
+                        <li><a class="facebook" href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                        <li><a class="twitter" href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                        <li><a class="linkedin" href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ home.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>E-commerce Project</title>
+  </head>
+  <body>
+  <nav class="navbar fixed-top navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="#"><img  src="images/logo.jpeg" height="80px" width="150px"  /> </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="main_nav">
+  	
+		<ul class="navbar-nav mx-auto">
+			<li class="nav-item active"> <a class="nav-link mx-4" href="#">Home </a> </li>
+			<li class="nav-item"><a class="nav-link mx-4" href="about.php"> About </a></li>
+			<li class="nav-item"><a class="nav-link mx-4" href="#">Contact us</a></li>
+            
+		</ul>
+        <ul style="list-style: none;">
+		<li class="nav-item"><a class="nav-link" href="#"><img src="images/add-to-basket.png" width="40px" height="40px"></a></li>
+        </ul>
+        <a class="btn btn-warning mx-4" href="#">Login</a>
+        <a class="btn btn-warning mx-4" href="#">Register</a>
+  </div> <!-- navbar-collapse.// -->
+
+  </div>
+</nav>
+
+<!-- <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <h6>About</h6>
+                    <p class="text-justify">Medico shop- Your Health & Fitness Partner is an online portal for healthcare products brought to you by Medicare Products Inc. The company is managed by a team of two young & motivated entrepreneurs with an objective to provide and extend personalized and innovative healthcare services to its customers.</p>
+                </div>
+
+                <div class="col-xs-6 col-md-3">
+                    <h6>Categories</h6>
+                    <ul class="footer-links">
+                        <li><a href="#">Syrup</a></li>
+                        <li><a href="#">Tablet</a></li>
+                        <li><a href="#">Ayurveda</a></li>
+                        <li><a href="#">Hompyopethik</a></li>
+                        <li><a href="#">Emulsions</a></li>
+                        <li><a href="#">Inhalers</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-xs-6 col-md-3">
+                    <h6>Quick Links</h6>
+                    <ul class="footer-links">
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+                        <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-sm-6 col-xs-12">
+                    <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by
+                        <a href="#">Scanfcode</a>.
+                    </p>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <ul class="social-icons">
+                        <li><a class="facebook" href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                        <li><a class="twitter" href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                        <li><a class="linkedin" href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer> -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
